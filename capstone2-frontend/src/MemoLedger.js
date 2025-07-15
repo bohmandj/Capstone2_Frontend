d5b4e5dce4bb0b719731a163b56793c9c86aee10
David Bohman <david.bohman.dev@gmail.com>
2025-07-14 18:34:13 -0600

    add basic main MemoLedger component to App

---
+++ capstone2-frontend/src/MemoLedger.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const MemoLedger = () => {
+    return (
+        <div className="MemoLedger">
+            <h1>Capstone 2</h1>
+        </div>
+    )
+}
+
+export default MemoLedger;
