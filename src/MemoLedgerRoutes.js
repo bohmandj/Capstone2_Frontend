d95a22f0587c8f1eec995ecaef864782c6adfd65
David Bohman <david.bohman.dev@gmail.com>
2025-07-15 21:46:05 -0600

    add Homepage & catchall routes

--- src/MemoLedgerRoutes.js
+++ src/MemoLedgerRoutes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, Routes, Navigate } from "react-router-dom";
 import LoginForm from './LoginForm';
+import Home from './Home';
 
 const MemoLedgerRoutes = () => {
     return (
         <Routes>
             <Route path="/login" element={<LoginForm />} />
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     )
 }
